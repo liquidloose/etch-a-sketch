6f3ff02892a9ff9913ec7db5dd091e91fc80dcbf
liquidloose <ronaldlussier83@gmail.com>
2022-01-31 22:30:36 +0000

    added mouseover event listeners and button event listener

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function mouseOver().addeventlister(mouseOver) {
-    const target = querySelector(square).classList.add('colored');
-    target.classList.add('colored');
-}
+const target = document.querySelectorAll('.square');
 
+target.forEach((item) => {
+  item.addEventListener('mouseover', () => {
+    item.classList.add('colors');
+  });
+});
 
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  const remove = document.querySelectorAll('.square');
+  remove.forEach((item) => {
+    item.classList.remove('colors');
+  });
+});

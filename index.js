1e5f4990222faad79605c06f875b90fff85afdce
liquidloose <ronaldlussier83@gmail.com>
2022-02-01 02:57:35 +0000

    fully functional drawing board. only writes in one color as of now

--- index.js
+++ index.js
@@ -1,16 +1,59 @@
-const target = document.querySelectorAll('.square');
+function addColor() {
+  const target = document.querySelectorAll('.square');
 
-target.forEach((item) => {
-  item.addEventListener('mouseover', () => {
+  target.forEach((item) => {
+    item.addEventListener('mouseover', () => {
+      item.classList.add('colors');
+    });
+  });
+}
+
+function resetButton() {
+  const button = document.querySelector('button');
+
+  button.addEventListener('click', () => {
+    const remove = document.querySelectorAll('.square');
+    remove.forEach((item) => {
+      item.classList.remove('colors');
+    });
+  });
+}
+
+const size = 50;
+
+function createRows() {
+  for (let i = 0; i < size; i += 1) {
+    const target = document.querySelector('.outer-container');
+    const div = document.createElement('div');
+    div.classList.add('row');
+
+    target.appendChild(div);
+    console.log(i);
+  }
+}
+
+function addClass() {
+  const content = document.querySelectorAll('.row');
+  content.forEach((item) => {
     item.classList.add('colors');
+    console.log(item);
   });
-});
+}
 
-const button = document.querySelector('button');
-
-button.addEventListener('click', () => {
-  const remove = document.querySelectorAll('.square');
-  remove.forEach((item) => {
-    item.classList.remove('colors');
+function fillRows() {
+  const rows = document.querySelectorAll('.row');
+  rows.forEach((item) => {
+    for (let j = 0; j < size; j += 1) {
+      const squares = document.createElement('div');
+      squares.classList.add('square');
+      item.appendChild(squares);
+    }
+    console.log(rows[item]);
   });
-});
+}
+
+createRows();
+addClass();
+fillRows();
+addColor();
+resetButton();
